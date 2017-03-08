eb011e2b6269e1dd70e4c72fadc4185e8e1f8d96
sirwilliamiv <wjconnolly2@gmail.com>
2017-03-08 11:48:47 -0600

    dynamically search job titles using process.argv

--- exersise13/main.js
+++ exersise13/main.js
@@ -2,17 +2,26 @@
 
 const sqlite3 = require('sqlite3').verbose(); // verbose helps track errors
 const db = new sqlite3.Database('employees.sqlite', (err) => console.log('Connected')); //or .db extension
+
+
+
+// Your friend has decided that they want to add a salary column to the employees table.
+// Make sure to add a salary key value pair to each of the employee objects.
+//  Then drop the existing employees table,
 const dropEmployees = () => {
-  db.run(`DROP TABLE employees`)
+ db.run(`DROP TABLE employees`)
 };
 // dropEmployees();
 
+//  update the schema to accept a salary column,
+//  and repopulate the table.
 db.run(`CREATE TABLE IF NOT EXISTS employees (id INT, first TEXT, last TEXT,address TEXT,title TEXT ,salary INT)`);
 
 
 
 // Write a statement to query the database and console.log() all employee records.
 const populateEmployees = () => {
+  // Instead of using an array in the .js file, create a JSON file of employees to require into the js file. Use this to populate the table.
     const { Employees } = require('./employees.json');
     // console.log(Employees)
     Employees.forEach(({ employeeCode, firstName, lastName, address, jobTitleName, salary }) => {
@@ -35,16 +44,30 @@ const jobTitle = () => {
     console.log(`${first} ${last}: ${title}`)
   })
 }
-jobTitle()
+// jobTitle();
 
 
 
 // Write a statement to query the database and console.log() each employees firstName, lastName and address only.
+const employeeDetails = () => {
+  db.each(`SELECT * FROM employees`, (err, { address, first, last }) => {
+    if(err) throw err
+    // console.log(employees)
+    console.log(`${first} ${last}: ${address}`)
+  })
+}
+// employeeDetails();
 
-// Bonus Features
 
-// Instead of using an array in the .js file, create a JSON file of employees to require into the js file. Use this to populate the table.
-
-// Your friend has decided that they want to add a salary column to the employees table. Make sure to add a salary key value pair to each of the employee objects. Then drop the existing employees table, update the schema to accept a salary column, and repopulate the table.
 
 // Write a statement that returns all employees of a specified jobTitle.
+let [,,...searchThisTitle] = process.argv
+
+const jobTitleSearch = (matchingTitle) => {
+
+  db.each(`SELECT * FROM employees
+            WHERE title = "${matchingTitle}"`, (err, { first, last, title} )=> {
+            console.log(`${first} ${last}: ${title}`)
+            })
+}
+jobTitleSearch(searchThisTitle.toString())
