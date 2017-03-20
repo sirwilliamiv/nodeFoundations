372b75b27a02aa02d2c72c63bba77256324da831
sirwilliamiv <wjconnolly2@gmail.com>
2017-03-20 14:55:13 -0500

    diceroll and sqlite

--- exercise19/lib/cli.js
+++ exercise19/lib/cli.js
@@ -3,3 +3,6 @@ const comObj = require('./parse-args')(commands);
 const  action = require('./action.js')
 
 console.log(action(comObj))
+
+// [name, address, zipcode, phonenumber] =  result
+// module.exports=
